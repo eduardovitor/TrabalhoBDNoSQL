749cc2d68d963266b2ca8d7416dd7f804d448ff1
Tamilly Nascimento <myllacontato@gmail.com>
2021-08-13 23:30:06 -0300

    Criação de ProfessorDAO

--- TrabalhoBDSQL/src/main/java/AlunoDAO.java
+++ TrabalhoBDSQL/src/main/java/AlunoDAO.java
@@ -56,21 +56,46 @@ public class AlunoDAO {
             e.printStackTrace();
         }
     }
-/*
-    public void alterar(Aluno a){
-        MongoDatabase bd = con.getBanco();
-        MongoCollection<Document> colecaoAluno = bd.getCollection("alunos");
-        Document doc = new Document();
-        doc.append("id", a.getId());
-        doc.append("nome", a.getNome());
-        doc.append("cpf", a.getCpf());
-        doc.append("data_matricula", a.getData_matricula());
-        doc.append("turma", a.getTurma());
-        doc.append("disciplinas", a.getDisciplinas());
-        doc.append("endereco", a.getEndereco());
-        bd.getCollection("alunos");
+
+    public void alterar(Aluno a, Aluno n){
+        try{
+            con = new MongoClient("localhost", 27017);
+            System.out.println("Conectado ao Banco. Inserindo...");
+            MongoDatabase bd = con.getDatabase("trabalho").withCodecRegistry(pojoCodecRegistry);
+            MongoCollection<Document> colecaoAluno = bd.getCollection("alunos");
+            Document doc = new Document();
+            doc.append("_id", a.getId());
+            doc.append("nome", a.getNome());
+            doc.append("cpf", a.getCpf());
+            doc.append("data_matricula", a.getData_matricula());
+            doc.append("turma", a.getTurma());
+            doc.append("disciplinas", a.getDisciplinas());
+            Document end = new Document();
+            end.append("rua", a.getEndereco().getRua());
+            end.append("bairro", a.getEndereco().getBairro());
+            end.append("numero", a.getEndereco().getNumero());
+            end.append("cidade", a.getEndereco().getCidade());
+            doc.append("endereco", end);
+            Document doc2 = new Document();
+            doc2.append("_id", n.getId());
+            doc2.append("nome", n.getNome());
+            doc2.append("cpf", n.getCpf());
+            doc2.append("data_matricula", n.getData_matricula());
+            doc2.append("turma", n.getTurma());
+            doc2.append("disciplinas", n.getDisciplinas());
+            Document end2 = new Document();
+            end2.append("rua", n.getEndereco().getRua());
+            end2.append("bairro", n.getEndereco().getBairro());
+            end2.append("numero", n.getEndereco().getNumero());
+            end2.append("cidade", n.getEndereco().getCidade());
+            doc2.append("endereco", end2);
+            colecaoAluno.updateOne(doc, doc2);
+            System.out.println("Aluno atualizado com sucesso.");
+        }catch(MongoException e){
+            e.printStackTrace();
+        }
     }
-*/
+
     public void deletar(int id){
         try{
             MongoDatabase bd = con.getDatabase("trabalho");
--- TrabalhoBDSQL/src/main/java/ProfessorDAO.java
+++ TrabalhoBDSQL/src/main/java/ProfessorDAO.java
@@ -4,32 +4,39 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
 import java.util.ArrayList;
 import java.util.List;
-/*
+
+
 public class ProfessorDAO {
 
-    private ConexaoBD con;
+    private MongoClient con;
+    public CodecRegistry pojoCodecRegistry = org.bson.codecs.configuration.CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), org.bson.codecs.configuration.CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
     public void inserir(Aluno a){
         try{
-            if(con.conectar()!=null){
-                System.out.println("Conectado ao Banco. Inserindo...");
-                MongoDatabase bd = con.getBanco();
-                MongoCollection<Document> colecaoAluno = bd.getCollection("alunos");
-                Document doc = new Document();
-                doc.append("id", a.getId());
-                doc.append("nome", a.getNome());
-                doc.append("cpf", a.getCpf());
-                doc.append("data_matricula", a.getData_matricula());
-                doc.append("turma", a.getTurma());
-                doc.append("disciplinas", a.getDisciplinas());
-                doc.append("endereco", a.getEndereco());
-                colecaoAluno.insertOne(doc);
-                System.out.println("Aluno inserido com sucesso.");
-            }else
-                System.out.println("Não foi possível conectar ao Banco de Dados.");
+            con = new MongoClient("localhost", 27017);
+            System.out.println("Conectado ao Banco. Inserindo...");
+            MongoDatabase bd = con.getDatabase("trabalho").withCodecRegistry(pojoCodecRegistry);
+            MongoCollection<Document> colecaoAluno = bd.getCollection("alunos");
+            Document doc = new Document();
+            doc.append("_id", a.getId());
+            doc.append("nome", a.getNome());
+            doc.append("cpf", a.getCpf());
+            doc.append("data_matricula", a.getData_matricula());
+            doc.append("turma", a.getTurma());
+            doc.append("disciplinas", a.getDisciplinas());
+            Document end = new Document();
+            end.append("rua", a.getEndereco().getRua());
+            end.append("bairro", a.getEndereco().getBairro());
+            end.append("numero", a.getEndereco().getNumero());
+            end.append("cidade", a.getEndereco().getCidade());
+            doc.append("endereco", end);
+            colecaoAluno.insertOne(doc);
+            System.out.println("Aluno inserido com sucesso.");
         }catch(MongoException e){
             e.printStackTrace();
         }
@@ -37,8 +44,8 @@ public class ProfessorDAO {
 
     public void mostrarTudo(){
         try{
-            con.conectar();
-            MongoDatabase bd = con.getBanco();
+            MongoClient test = new MongoClient("localhost", 27017);
+            MongoDatabase bd = test.getDatabase("trabalho");
             MongoCollection<Document> colecaoAluno = bd.getCollection("alunos");
 
             MongoCursor cursor = colecaoAluno.find().cursor();
@@ -50,28 +57,56 @@ public class ProfessorDAO {
         }
     }
 
-    public void alterar(Aluno a){
-        MongoDatabase bd = con.getBanco();
-        MongoCollection<Document> colecaoAluno = bd.getCollection("alunos");
-        Document doc = new Document();
-        doc.append("id", a.getId());
-        doc.append("nome", a.getNome());
-        doc.append("cpf", a.getCpf());
-        doc.append("data_matricula", a.getData_matricula());
-        doc.append("turma", a.getTurma());
-        doc.append("disciplinas", a.getDisciplinas());
-        doc.append("endereco", a.getEndereco());
-        bd.getCollection("alunos");
+    public void alterar(Aluno a, Aluno n){
+        try{
+            con = new MongoClient("localhost", 27017);
+            System.out.println("Conectado ao Banco. Inserindo...");
+            MongoDatabase bd = con.getDatabase("trabalho").withCodecRegistry(pojoCodecRegistry);
+            MongoCollection<Document> colecaoAluno = bd.getCollection("alunos");
+            Document doc = new Document();
+            doc.append("_id", a.getId());
+            doc.append("nome", a.getNome());
+            doc.append("cpf", a.getCpf());
+            doc.append("data_matricula", a.getData_matricula());
+            doc.append("turma", a.getTurma());
+            doc.append("disciplinas", a.getDisciplinas());
+            Document end = new Document();
+            end.append("rua", a.getEndereco().getRua());
+            end.append("bairro", a.getEndereco().getBairro());
+            end.append("numero", a.getEndereco().getNumero());
+            end.append("cidade", a.getEndereco().getCidade());
+            doc.append("endereco", end);
+            Document doc2 = new Document();
+            doc2.append("_id", n.getId());
+            doc2.append("nome", n.getNome());
+            doc2.append("cpf", n.getCpf());
+            doc2.append("data_matricula", n.getData_matricula());
+            doc2.append("turma", n.getTurma());
+            doc2.append("disciplinas", n.getDisciplinas());
+            Document end2 = new Document();
+            end2.append("rua", n.getEndereco().getRua());
+            end2.append("bairro", n.getEndereco().getBairro());
+            end2.append("numero", n.getEndereco().getNumero());
+            end2.append("cidade", n.getEndereco().getCidade());
+            doc2.append("endereco", end2);
+            colecaoAluno.updateOne(doc, doc2);
+            System.out.println("Aluno atualizado com sucesso.");
+        }catch(MongoException e){
+            e.printStackTrace();
+        }
     }
 
     public void deletar(int id){
-        MongoDatabase bd = con.getBanco();
-        MongoCollection<Document> colecaoAluno = bd.getCollection("alunos");
-        Document consulta = new Document();
-        consulta.put("id", id);
-        DeleteResult resultado = colecaoAluno.deleteMany(consulta);
-        System.out.println("Número de documentos deletados: " + resultado.getDeletedCount());
+        try{
+            MongoDatabase bd = con.getDatabase("trabalho");
+            MongoCollection<Document> colecaoAluno = bd.getCollection("alunos");
+            Document consulta = new Document();
+            consulta.append("_id", id);
+            DeleteResult resultado = colecaoAluno.deleteMany(consulta);
+            System.out.println("Número de documentos deletados: " + resultado.getDeletedCount());
+        }catch(MongoException e){
+            e.printStackTrace();
+        }
+
     }
 }
-
-}*/
